bae42f1a7c570bf57e6ffee038fcc2d89e78e9e1
Viet NT <tienvietnguyen1110@gmail.com>
2021-08-08 23:41:00 +0700

    update map plugin in

--- wp-content/plugins/sentius-map/sentius-map.php
+++ wp-content/plugins/sentius-map/sentius-map.php
@@ -37,7 +37,7 @@
 class Sentius_Map
 {
     private static $_instance;
-    public $author_name = '';
+    private static $initiated = false;
     public static function get_instance()
     {
         if (!is_null(self::$_instance)) {
@@ -49,12 +49,94 @@ class Sentius_Map
     {
         // Do stuff
     }
-    public function set_author_name($author_name)
+    public static function init()
     {
-        $this->author_name = $author_name;
+        if (!self::$initiated) {
+            self::init_hooks();
+        }
     }
-    public function get_author_name()
+
+    private static function init_hooks()
     {
-        return $this->author_name;
+        self::$initiated = true;
+        add_action('admin_menu', array('Sentius_Map', 'vnt_option'));
+        add_action('init', array('Sentius_Map', 'vnt_add_settings'));
+    }
+
+    public static function vnt_option()
+    {
+        add_menu_page('VNT Map', 'Map Settings', 'administrator', 'vnt_option', 'vnt_adjustments', 'dashicons-location-alt', 99);
+    }
+    public static function vnt_add_settings()
+    {
+        register_setting('vnt_gmap_setting', 'vnt_gmap_latitude');
+        register_setting('vnt_gmap_setting', 'vnt_gmap_longitude');
     }
 }
+
+add_action('init', array('Sentius_Map', 'init'));
+
+
+function vnt_adjustments()
+{
+?>
+    <div class="wrap">
+        <h1>Google Map Settings</h1>
+        <form action="options.php" method="post">
+            <?php
+            settings_fields('vnt_gmap_setting');
+            do_settings_sections('vnt_gmap_setting');
+            $latitude = get_option('vnt_gmap_latitude') ? get_option('vnt_gmap_latitude') : 21.029039423363564;
+            $longitude = get_option('vnt_gmap_longitude') ? get_option('vnt_gmap_longitude') : 105.85208456787406;
+            ?>
+            <table class="form-table">
+                <tr>
+                    <th scope="row">Latitude : </th>
+                    <td>
+                        <input required placeholder="Location's latitude here like: 40.6773988" class="regular-text" type="text" name="vnt_gmap_latitude" value="<?php echo esc_attr($latitude); ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">Longitude : </th>
+                    <td>
+                        <input required placeholder="Location's longitude here like: -85.6134087" class="regular-text" type="text" name="vnt_gmap_longitude" value="<?php echo esc_attr($longitude); ?>">
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+        <br>
+        <h2>Instructions:</h2>
+        <p>Shortcode <span style="color:red; font-weight:bold;">[vnt-map]</span></p>
+    </div>
+<?php
+} ?>
+
+<?php
+function vnt_create_map()
+{
+    ob_start(); ?>
+    <?php
+
+    $latitude = get_option('vnt_gmap_latitude') ? get_option('vnt_gmap_latitude') : 21.029039423363564;
+    $longitude = get_option('vnt_gmap_longitude') ? get_option('vnt_gmap_longitude') : 105.85208456787406;
+
+    if (get_option('vnt_gmap_height')) {
+        $getHeight = (int) filter_var(get_option('vnt_gmap_height'), FILTER_SANITIZE_NUMBER_INT);
+        $height = $getHeight . "px";
+    } else {
+        $height = "400px";
+    }
+
+    $width = get_option('vnt_gmap_width') ? get_option('vnt_gmap_width') : "100%";
+    ?>
+    <div id="vnt-map" style="overflow:hidden; width:<?php echo $width; ?>; margin:0 auto; position: relative;">
+        <div class="fluid-width-video-wrapper" style="padding-top:1px !important;">
+            <iframe src="https://maps.google.com/maps?q=<?php echo $latitude; ?>,<?php echo $longitude; ?>&hl=en;z=14&amp;output=embed" width="100%" frameborder="0" title="Our Location in google map" style="height:<?php echo $height; ?>; width:100%;  padding:0 !important;" allowfullscreen="">
+            </iframe>
+        </div>
+    </div>
+<?php $content = ob_get_clean();
+    return $content;
+}
+add_shortcode('vnt-map', 'vnt_create_map');
