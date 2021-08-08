10e1d2ac9143595d106403abe6954301192b6146
Viet NT <tienvietnguyen1110@gmail.com>
2021-08-08 22:01:38 +0700

    ci: add test

---
+++ wp-content/plugins/sentius-map/tests/test-sentius.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SentiusTest
+ *
+ * @package Sentius_Map
+ */
+
+/**
+ * Sample test case.
+ */
+class SentiusTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
